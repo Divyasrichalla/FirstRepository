e4a89788e8344b2688d5fd1b57ba76fde50c1556
Divya sri <challadivyasri94@gmail.com>
2021-06-07 20:23:56 +0530

    name changed

--- Employeedata.java
+++ Employeedata.java
@@ -9,11 +9,11 @@ class Employeedata{
 	{
 		Employee e1 = new Employee();
 		Employee e2 = new Employee();
-		e1.id = 401;
-		e1.name = "Naveen";
+		e1.id = 407;
+		e1.name = "Venkata Naveen";
 		e1.salary = 100000;
 		e2.id = 402;
-		e2.name = "Divya";
+		e2.name = "Divya Sri";
 		e2.salary = 75000;
 	System.out.println(e1.id+ " " +e1.name+ " " + e1.salary);
 	System.out.println(e2.id+ " " +e2.name+ " " + e2.salary);
